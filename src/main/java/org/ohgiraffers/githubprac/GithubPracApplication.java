e115ef764fca3d40bf77077a83f27a94a1748dea
DESKTOP-IFMN175\804-03 <mangoperry2015@gmail.com>
2024-02-16 09:52:41 +0900

    organization first commit practice

---
+++ src/main/java/org/ohgiraffers/githubprac/GithubPracApplication.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.githubprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubPracApplication.class, args);
+    }
+
+}
